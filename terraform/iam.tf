8fa7ccd8cc17048d318b89beb5e0529f17f4c2f3
Igor Rospasienko <igorrosp@gmail.com>
2026-09-07 20:50:15 +0300

    Replaced static target configuration in Prometheus with EC2 Service Discovery

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -19,6 +19,7 @@ module "ec2_iam_role" {
   policies = {
     AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
     CloudWatchAgentServerPolicy  = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+    AmazonEC2ReadOnlyAccess      = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
   }
 
   tags = {
